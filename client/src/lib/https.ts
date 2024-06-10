424c378ba659cabb5fdea659656b695907b883ff
Chau Dang <dnchau2310@gmail.com>
2024-06-10 23:22:48 +0700

    http

---
+++ client/src/lib/https.ts
@@ -0,0 +1,60 @@
+import envConfig from "../../config";
+
+type CustomOptions = RequestInit & {
+    baseUrl?: string | undefined;
+}
+
+class HttpError extends Error {
+  status: number;
+  payload: any;
+  constructor({ payload, status }: { status: number; payload: any }) {
+    super("Http Error");
+    this.status = status;
+    this.payload = payload;
+  }
+}
+
+
+const request = async <Response>(method: 'GET' | 'POST' | 'DELETE' | 'PUT', url: string, options?: CustomOptions) => {
+    const body = options?.body ? JSON.stringify(options.body) : undefined;
+    const headers = {
+        'Content-Type': 'application/json',
+        ...options?.headers,
+    };
+    const baseUrl = options?.baseUrl === undefined ? envConfig.NEXT_PUBLIC_API_ENDPOINT : options.baseUrl;
+    const fullUrl = url.startsWith('/') ? `${baseUrl}${url}` : `${baseUrl}/${url}`;
+
+    const res = await fetch(fullUrl, {
+        ...options,
+        headers,
+        body,
+        method
+    })
+
+    const payload: Response = await res.json();
+    const data = {
+        status: res.status,
+        payload,
+    };
+
+    if(!res.ok) {
+        throw new HttpError(data);
+    }
+
+    return data;
+}
+
+const http = {
+    get<Response>(url: string, options?: Omit<CustomOptions, 'body'> | undefined) {
+        return request<Response>('GET', url, options);
+    },
+    post<Response>(url: string, body: any, options?: Omit<CustomOptions, 'body'> | undefined) {
+        return request<Response>('POST', url, {...options, body});
+    },
+    delete<Response>(url: string, options?: Omit<CustomOptions, 'body'> | undefined) {
+        return request<Response>('DELETE', url, options);
+    },
+    put<Response>(url: string, options?: CustomOptions) {
+        return request<Response>('PUT', url, options);
+    }
+}
--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,10 +1,21 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const privatePaths = ["/me"];
+const authPaths = ["/login", "/register"];
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const {pathname} = request?.nextUrl
   const sessionToken = request.cookies.get("sessionToken")?.value;
+  // Check if the request is for a private path and the user is not authenticated
+  if (privatePaths.includes(pathname) && !sessionToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  // Check if the request is for an auth path and the user is authenticated
+  if (authPaths.includes(pathname) && sessionToken) {
+    return NextResponse.redirect(new URL('/me', request.url));
+  } 
 
   return NextResponse.next();
 }
